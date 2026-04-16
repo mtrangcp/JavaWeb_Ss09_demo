5eb5c6bd6568bb7e48b245e2d3d04818768b3dee
mtrangcp <haminhtrang545@gmail.com>
2026-04-16 09:48:38 +0700

    mm

--- src/main/java/com/demo/ss9/config/I18NConfiguration.java
+++ src/main/java/com/demo/ss9/config/I18NConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
@@ -19,7 +20,7 @@ public class I18NConfiguration implements WebMvcConfigurer {
     public MessageSource messageSource() {
         ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
         messageSource.setDefaultEncoding("UTF-8");
-        messageSource.setBasename("classpath:language");
+        messageSource.setBasename("language");
         return messageSource;
     }
 
